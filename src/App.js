1947e5a2399576922e62933c6ad8c2b7831584d6
whiausbon214 <whiausbon@outlook.com>
2025-01-31 03:32:17 -0600

    Carbon Design System

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import AppNavbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
 import DistributionGroups from "./pages/DistributionGroups";
 import CloudStatus from "./pages/CloudStatus"; // Import the CloudStatus component
-import '@fontsource/inter';
+import '@ibm/plex-sans';
 
 function App() {
   return (
